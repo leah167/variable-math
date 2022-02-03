94234325ae3e9b792e0f0ebc1b04f3eaf045a83a
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-03 16:38:35 -0700

    quadratic solution

---
+++ quadratic.js
@@ -0,0 +1,7 @@
+let a = 1
+let b = 4
+let c = 3
+let x1 = (-b + (Math.sqrt((Math.pow (b, 2)) - 4*a*c)))/(2*a)
+console.log(x1)
+let x2 = (-b - (Math.sqrt((Math.pow (b, 2)) - 4*a*c)))/(2*a)
+console.log(x2)
