7d7ad69691e84fb24c00f9e37451d844cb1185eb
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-02-03 15:47:34 -0700

    distance solution

---
+++ distance.js
@@ -0,0 +1,6 @@
+let x1 = 2
+let y1 = 4
+let x2 = 8
+let y2 = 10
+let D = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2))
+console.log("The distance between points (" + x1 +", " + y1 + ") and (" + x2 +", " + y2 +") is equal to " + D + "." )
